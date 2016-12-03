c97b2b24970791fcd15b2890c1ccc2caeae8651b
Saico <elsaico@partidopirata.com.ar>
2016-12-02 21:03:15 -0300

    Diferenciar comentarios de posturas

--- slacker_uprising.rb
+++ slacker_uprising.rb
@@ -7,13 +7,14 @@ require 'carrier-pigeon'
 
 post "/" do
 hash=JSON.parse(request.env["rack.request.form_vars"])
-msj=hash["attachments"].first["text"]
-user=hash["text"].split('u/').last.split('|').first
+#msj=hash["attachments"].first["text"].split("\n").first
+msj=hash["text"].gsub(/<[^|]+\|/,"").gsub(">","")
+#user=hash["text"].split('u/').last.split('|').first
 thread=hash["text"].split('<').last.split('|').first
 Thread.new do
   CarrierPigeon.send(
     uri: "irc://starhawk@irc.pirateirc.net:6697/#ppar",
-    message: "#{user} dijo #{msj} en #{thread}", 
+    message: "#{msj} en #{thread}", 
     ssl: true,
     join: true,
   )
